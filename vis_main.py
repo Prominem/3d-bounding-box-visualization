868419c2e45da3716773824b9263025d1c94c6c2
Prominem <31801679+Prominem@users.noreply.github.com>
2020-02-13 13:30:39 +0800

    Update vis_main.py

--- vis_main.py
+++ vis_main.py
@@ -1,12 +1,22 @@
 from draw3d_box import *
 import numpy as np
-# **********************main*********************
-folder_dir = 'D:\\study\\python_codes\\data\\sunrgbd\\'
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(BASE_DIR, 'data')
+vis_dir = os.path.join(BASE_DIR, 'vis')
+if not os.path.exists(vis_dir):
+    os.makedirs(vis_dir)
 scene = '005050'
 box_name = scene + '_bbox.npy'
-box_dir = folder_dir + box_name
+pts_name = scene + '_pc.npz'
+box_dir = os.path.join(data_dir, box_name)
+pts_dir = os.path.join(data_dir, pts_name)
+out_box_dir = os.path.join(vis_dir, scene+'_box.txt')
+out_pts_dir = os.path.join(vis_dir, scene+'_pts.txt')
+points = np.load(pts_dir)['pc']  # (n, 6)
 box_paras = np.load(box_dir)  # (n, 8)
 box_paras[:, 3:6] *= 2
 box_points = generate_3d_box(box_paras, 0.01)
-out_dir = folder_dir +scene + '_bbox.txt'
-np.savetxt(out_dir, box_points)
+np.savetxt(out_box_dir, box_points)
+np.savetxt(out_pts_dir, points)
